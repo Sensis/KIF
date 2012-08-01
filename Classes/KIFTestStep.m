83c0761a92818988396fe5b62f648c344a5a0f1c
Chaise Hocking <chaise.hocking@sensis.com.au>
2012-08-01 12:44:11 +1000

    Made tableview scroll to move indexPath into view if tapping indexPath not visible in tableView

--- Classes/KIFTestStep.m
+++ Classes/KIFTestStep.m
@@ -615,6 +615,14 @@ typedef CGPoint KIFDisplacement;
         KIFTestCondition(tableView, error, @"Table view with label '%@' not found", tableViewLabel);
 
         UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        if (!cell) {
+            KIFTestCondition([indexPath section] < [tableView numberOfSections], error, @"Section %d is not found in '%@' table view", [indexPath section], tableViewLabel);
+            KIFTestCondition([indexPath row] < [tableView numberOfRowsInSection:[indexPath section]], error, @"Row %d is not found in section %d of '%@' table view", [indexPath row], [indexPath section], tableViewLabel);
+            [tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionMiddle animated:YES];
+            [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.25]];
+            cell = [tableView cellForRowAtIndexPath:indexPath];
+        }
+        
 		KIFTestWaitCondition(cell, error, @"Cell at indexPath '%@' not found", [indexPath description]);
 
         [cell tapAtPoint:[cell tappablePointInRect:cell.bounds]];
